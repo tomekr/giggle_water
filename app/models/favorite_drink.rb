118cb44209eb23d76c33706fd88e79872b0147b1
mostlybadfly <reachdanielnunez@gmail.com>
2015-07-05 07:38:03 -0500

    Tests for favorite drinks

--- app/models/favorite_drink.rb
+++ app/models/favorite_drink.rb
@@ -1,4 +1,7 @@
 class FavoriteDrink < ActiveRecord::Base
   belongs_to :drink
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :drink_id, presence:  true
 end
--- test/models/favorite_drink_test.rb
+++ test/models/favorite_drink_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class FavoriteDrinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "favorite must have a user id" do
+    user = User.new
+    favorite_drink = user.favorite_drinks.new
+    assert favorite_drink.invalid?
+    assert favorite_drink.errors[:user_id].any?
+  end
+
+  test "favorite must have a drink id" do
+    user = User.new
+    favorite_drink = user.favorite_drinks.new
+    assert favorite_drink.invalid?
+    assert favorite_drink.errors[:drink_id].any?
+  end
 end
