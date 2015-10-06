cd168ba5b3ae85d3b5bf2ba922ab9842d3b924de
Daniel Nunez <reachdanielnunez@gmail.com>
2015-10-05 20:27:26 -0500

    Added ingredient params for Ingredient#create test

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -90,6 +90,6 @@ class IngredientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def ingredient_params
-      params[:ingredient]
+      params.require(:ingredient).permit(:name, :ingredient_type)
     end
 end
