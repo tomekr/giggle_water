9c0e3a2e6465a2caff6fa8d728d5cb27d8e4d596
Daniel Nunez <reachdanielnunez@gmail.com>
2015-12-07 18:45:49 -0600

    SearchController inherits from ApplicationController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,4 @@
-class SearchController < ActionController::Base
+class SearchController < ApplicationController
   def search
     @drink_results = Search.drink_search(params[:search])
     @ingredient_results = Search.ingredient_search(params[:search])
