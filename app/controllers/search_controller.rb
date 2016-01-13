bd5ffa5519253b53c518406374bcaaa1a6732d45
Daniel Nunez <reachdanielnunez@gmail.com>
2016-01-13 13:38:37 -0600

    update search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,24 @@
 class SearchController < ApplicationController
   def search
-    @drink_results = Search.drink_search(params[:search])
-    @ingredient_results = Search.ingredient_search(params[:search])
+    @drink_results = drink_search(params[:search])
+    @ingredient_results = ingredient_search(params[:search])
+  end
+
+  def drink_name_search(search)
+    Drink.where("name LIKE ?", "%#{search}%")
+  end
+  
+  def ingredient_search(search)
+    Ingredient.where("name LIKE ?", "%#{search}")
+  end
+
+  def drink_search(search)
+    drink_results = drink_name_search(search)
+    ingredient_search(search).each do |ingredient|
+      ingredient.drinks.each do |drink|
+        drink_results << drink
+      end
+    end
+    drink_results
   end
 end
